3a4750f851a5f8affcb558cfbffc13e5ff4ab1f5
vurachaitanya <vurachaitanya@users.noreply.github.com>
2017-02-16 20:00:15 +0530

    Update sister_age.py

--- sister_age.py
+++ sister_age.py
@@ -1,4 +1,4 @@
-#~/usr/bin/python
+#!/usr/bin/python
 sister = 10
 brother = 12
 
@@ -6,3 +6,15 @@ if sister > brother:
     print "sister is older"
 else :
     print "brother is older"
+print "+++++++++++++++++++++++++++++"
+#!/usr/bin/python
+darpad=3
+nidhi=5
+if darpad > nidhi:
+    print "Darpad is elder"
+else:
+    print "Nidhi is elder"
+if darpad > 1 and nidhi > 4:
+    print "nidhi and Darpad"
+    print "a" in "nidhi"
+print "+++++++++++++++++++++++++++++++++"
