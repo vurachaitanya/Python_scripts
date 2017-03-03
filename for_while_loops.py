5e4e1c09430225c4a25f9665dfe69d7f6298484a
vurachaitanya <vurachaitanya@users.noreply.github.com>
2017-03-03 16:46:10 +0530

    For and while loop examples

---
+++ for_while_loops.py
@@ -0,0 +1,10 @@
+name = raw_input('type chaitanya else you will not get any output')
+while name == 'chaitanya':
+    print 'naaaaaaaaaaaaaaaaaaaaaaao'
+
+
+for number in [1,2,3,4]:
+    print ('chaitanya counts starts', number)
+
+for numbers in range(10):
+    print ('numbers are', numbers)
