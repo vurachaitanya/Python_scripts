07e6993a1eddf558621dd0c6557c0469b18d52a0
vurachaitanya <vurachaitanya@users.noreply.github.com>
2017-04-06 16:53:34 +0530

    python if and  condition
    python if and  condition

---
+++ if_and_or.py
@@ -0,0 +1,19 @@
+#n odd weird
+#n>2 and n<5 not weird
+#n even n>6 and n<20 weird
+#n even n>20 not weird
+
+#!/usr/bin/python
+#def main ()
+if __name__ =='__main__':
+    n = int(raw_input())
+    m = n%2
+    if m == 0:
+        if n >= 2 and n <= 5 :
+            print "Not Weird"
+        elif n >= 6 and n <= 20:
+            print "Weird"
+        elif n > 20 :
+            print "Not Weird"
+    else :
+        print "Weird"
